278b17b5e0d162d80b56fe3525039675e479ed6e
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-04-08 17:21:24 -0500

    Booleans and operators - part 2

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -2,16 +2,17 @@
 Console.WriteLine("[S]ee all Todos.");
 Console.WriteLine("[A]dd a Todo.");
 Console.WriteLine("[R]emove a Todo.");
-Console.WriteLine("[E]xit.");
+Console.WriteLine("[E]xit.\n");
 
 string userInput = Console.ReadLine();
 
 // Section 2, Lesson 27 - boolean type
-bool someBoolean = true; // explicitly boolean declaration and initialization
-var anotherBoolean = false; // implcit boolean declaration and initialization
-
-// Another example
 bool isUserInputABC = userInput == "ABC";
+
+// S2, L 28
+var number = 10;
+var isLargerThan4AndSmallerThan9 = number > 4 && number < 9; // false
+var isLargerThan4OrSmallerThan9 = number > 4 || number < 9; // true
 
 // pauses program running until the user presses any keyboard key
 Console.ReadKey(); 
