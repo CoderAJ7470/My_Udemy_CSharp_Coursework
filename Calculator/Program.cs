e1ae42e59b35bb94d580bdc9c27ff3fe49c078de
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-04-12 20:09:43 -0500

    Wrote my first program of the C# course. A very simple calculator. It works!

---
+++ Calculator/Program.cs
@@ -0,0 +1,80 @@
+﻿/**
+ * Create a simple calculator that takes two integers from user input and returns the sum of both.
+ * The user can enter "A" or "a" for addition, and similar for subtraction and multiplication.
+ * If the user enters an invalid choice for the operation or leaves that blank, the program should let the user know as such and still allow the user to terminate the program by pressing any key.
+ */
+
+int integerA = 0;
+int integerB = 0;
+
+void getUserInput()
+{
+    var userInputA = "";
+    var userInputB = "";
+
+    Console.WriteLine("Hello! Please enter your first number:");
+    userInputA = Console.ReadLine();
+
+    if (userInputA != "") {
+        integerA = int.Parse(userInputA);
+    }
+
+    Console.WriteLine("Please enter your second number:");
+    userInputB = Console.ReadLine();
+
+    if (userInputB != ""){
+        integerB = int.Parse(userInputB);
+    }
+
+    if (userInputA == null || userInputB == null) {
+        Console.WriteLine("Sorry, you did not enter one or more numbers. Please try again.");
+    }
+}
+void getUserChoice()
+{
+    var userChoice = "";
+
+    Console.WriteLine("What operation would you like to perform with " + integerA + " and " + integerB + "?");
+    Console.WriteLine("Please indicate by specifying with one of the first letters below:\n");
+    Console.WriteLine("[A]dd");
+    Console.WriteLine("[S]ubtract");
+    Console.WriteLine("[M]ultiply\n");
+
+    userChoice = Console.ReadLine();
+
+    performOperation(userChoice.ToLower());
+}
+void performOperation(string userInput)
+{
+    if (userInput == "a")
+    {
+        var sum = integerA + integerB;
+        showOperationSummary("sum", integerA, integerB, sum);
+    }
+    else if (userInput == "s")
+    {
+        var difference = integerA - integerB;
+        showOperationSummary("difference", integerA, integerB, difference);
+    }
+    else if (userInput == "m")
+    {
+        var product = integerA * integerB;
+        showOperationSummary("product", integerA, integerB, product);
+    }
+    else
+    {
+        Console.WriteLine("Sorry, invalid option specified. Please check your option and try again.");
+    }
+}
+void showOperationSummary(string operationType, int firstInteger, int secondInteger, int result)
+{
+    Console.WriteLine("You chose " + operationType);
+    Console.WriteLine("The " + operationType + " of " + firstInteger + " and " + secondInteger + " is " + result);
+}
+
+getUserInput();
+getUserChoice();
+
+Console.WriteLine("\nPress any key to close this window.\n");
+
+Console.ReadKey();
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,18 +1,9 @@
-﻿Console.WriteLine("My first line of C#.");
-Console.WriteLine("[S]ee all Todos.");
-Console.WriteLine("[A]dd a Todo.");
-Console.WriteLine("[R]emove a Todo.");
-Console.WriteLine("[E]xit.\n");
+﻿Console.WriteLine("Please enter a number: ");
 
 string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
 
-// Section 2, Lesson 27 - boolean type
-bool isUserInputABC = userInput == "ABC";
-
-// Sec. 2, L 28
-var number = 10;
-var isLargerThan4AndSmallerThan9 = number > 4 && number < 9; // false
-var isLargerThan4OrSmallerThan9 = number > 4 || number < 9; // true
+Console.WriteLine("You entered the number " + number);
 
 // pauses program running until the user presses any keyboard key
-Console.ReadKey(); 
+Console.ReadKey();
