f2770d1dd0e180a3273a3d7130f6f5e7e5a02866
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-05-07 16:36:37 -0500

    Added some overloaded methods; also learned expression-bodied methods

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -27,7 +27,7 @@ class MedicalAppointment
     // Overloaded constructor #1 of MedicalAppointment constructor above
     // calls the constructor below in a shorthand format using the "this" keyword to refer to
     // the constructor below, and then passing the pertinent arguments to the method
-    // signature below
+    // signature below. This reduces code duplication.
     public MedicalAppointment(string patientName): this(patientName, 7)
     {        
     }
@@ -39,12 +39,24 @@ class MedicalAppointment
         _date = DateTime.Now.AddDays(daysFromNow);
     }
 
+    // Overloaded method
     public void Reschedule(DateTime date)
     {
         _date = date;
         var print = new MedicalAppointmentPrint();
 
         print.Print(this);
+    }
+
+    // Overload #1
+    public void RescheduleByMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void AddMonthsAndDaysToAppointment(int monthsToAdd, int daysToAdd)
+    {
+        _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
     }
 }
 
