3897e352852d20551260a1dac3572befaf0df3af
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-05-19 15:45:03 -0500

    Static methods and properties, part 2 - now with the object instantiations removed

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,15 +1,8 @@
 ﻿// Section 3, Lesson 89 - Static classes and static methods
 
-var calculator = new Calculator();
-
 Console.WriteLine($"1 + 2 equals {Calculator.Add(1, 2)}");
 Console.WriteLine($"1 - 2 equals {Calculator.Subtract(1, 2)}");
 Console.WriteLine($"1 * 2 equals {Calculator.Multiply(1, 2)}");
-
-// Note that both this object and the one above are indentical - this class is a stateles class, since it only has methods and no fields
-var calculatorTwo = new Calculator();
-
-Console.WriteLine($"34 + 678 equals {Calculator.Add(34, 678)}");
 
 Console.ReadKey();
 
