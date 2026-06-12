abe9a1c60e9895f3c0928fa916bfc64cdda7a825
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-06-12 13:18:54 -0500

    Fully refactored the Names class to meet the SRP
    This meant putting the rest of the code into separate classes, each one meeting the SRP
    
    Also learned what namespaces and global namespaces are, and how to import them in other files

---
+++ SRP_AfterRefactoring/DataAccess/StringsTextualRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// File-scoped namespace declaration - you don't need to have the class inside the brace brackets - in C# V10 and newer
+namespace SRPAfterRefactoring.DataAccess;
+
+class StringsTextualRepository
+{
+    private static readonly string Separator = Environment.NewLine;
+
+    public List<string> Read(string filePath)
+    {
+        var fileContents = File.ReadAllText(filePath);
+        return fileContents.Split(Environment.NewLine).ToList();
+    }
+
+    public void Write(string filePath, List<string> nameStrings) =>
+        File.WriteAllText(filePath, string.Join(Environment.NewLine, nameStrings));
+}
+
---
+++ SRP_AfterRefactoring/NamesFormatter.cs
@@ -0,0 +1,5 @@
+﻿public class NamesFormatter
+{
+    public string Format(List<string> names) =>
+        string.Join(Environment.NewLine, names);
+}
